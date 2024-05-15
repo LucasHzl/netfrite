e4e24109440b68e50da8bb8d9b472cece5f4490b
LucasHzl <hezil.lucas@gmail.com>
2024-05-15 12:26:45 +0200

    initial react + next + navbar + carousels (wip)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return(
+        <div className="flex justify-center items-center w-screen h-20 bg-red-600"></div>
+    )
+}
---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,5 @@
+export default function HeroBanner() {
+    return(
+        <div className="w-screen h-96 flex justify-center items-center border-2 border-red-600 mt-8 mb-8">je suis la bannière</div>
+    )
+}
